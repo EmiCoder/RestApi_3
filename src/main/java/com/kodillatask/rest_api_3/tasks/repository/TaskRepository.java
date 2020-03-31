1e95073a5687f38ed1307adaa4f6c80ba22c8a91
Emilia <emicoder@interia.pl>
2020-03-31 19:13:02 +0200

    updated

--- src/main/java/com/kodillatask/rest_api_3/tasks/repository/TaskRepository.java
+++ src/main/java/com/kodillatask/rest_api_3/tasks/repository/TaskRepository.java
@@ -1,7 +1,6 @@
 package com.kodillatask.rest_api_3.tasks.repository;
 
 import com.kodillatask.rest_api_3.tasks.domain.task.Task;
-import com.kodillatask.rest_api_3.tasks.domain.task.TaskDto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
