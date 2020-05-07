bd8354528d248ebc0190acc1eb9be18eeb278130
Emilia <emicoder@interia.pl>
2020-05-07 19:52:08 +0200

    Zadanie: operacje matematyczne w Thymeleaf

--- src/main/java/com/kodillatask/rest_api_3/tasks/service/SimpleEmailService.java
+++ src/main/java/com/kodillatask/rest_api_3/tasks/service/SimpleEmailService.java
@@ -9,12 +9,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import javax.mail.*;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
+
 
 @Service
 public class SimpleEmailService {
