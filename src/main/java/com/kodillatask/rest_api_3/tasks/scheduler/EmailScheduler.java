36bdf473ea8a47a4acc7ad0ea5706964974d3844
Emilia <emicoder@interia.pl>
2020-05-11 16:27:41 +0200

    komentarz

--- src/main/java/com/kodillatask/rest_api_3/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/kodillatask/rest_api_3/tasks/scheduler/EmailScheduler.java
@@ -21,7 +21,7 @@ public class EmailScheduler {
     private AdminConfig adminConfig;
 
 
-//    @Scheduled(cron = "0 0 10 * * *")
+    @Scheduled(cron = "0 0 10 * * *")
     public void sendInformationEmail() {
         long size = taskRepository.count();
         simpleEmailService.send(new Mail(
--- src/main/java/com/kodillatask/rest_api_3/tasks/service/TrelloService.java
+++ src/main/java/com/kodillatask/rest_api_3/tasks/service/TrelloService.java
@@ -38,5 +38,7 @@ public class TrelloService {
 
 
         return newCard;
+
+        //fhgdgfsfda
     }
 }
