479cb5845ad9b752edc7c485b30241844a8581c3
Emilia <emicoder@interia.pl>
2020-05-11 18:40:18 +0200

    Zadanie: Wykonaj szablon maila

--- src/main/java/com/kodillatask/rest_api_3/tasks/service/MailCreatorService.java
+++ src/main/java/com/kodillatask/rest_api_3/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -14,10 +17,18 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     @Autowired
+    private TaskService taskService;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows you to send tasks to Trello service");
 
         Context context = new Context();
         context.setVariable("message", message);
@@ -28,7 +39,29 @@ public class MailCreatorService {
         context.setVariable("company_goal", adminConfig.getCompanyGoal());
         context.setVariable("company_email", adminConfig.getCompanyEmail());
         context.setVariable("company_phone", adminConfig.getCompanyPhone());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String informUserAboutNumberOfTasks(String message) {
+        int tasksQuantity = taskService.getAllTasks().size();
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("quantity", tasksQuantity);
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("goodbye","Best regards!" );
+        context.setVariable("preview_message", "Tasks Status" );
+        return templateEngine.process("mail/information-tasks-quantity-mail", context);
     }
 
 }
